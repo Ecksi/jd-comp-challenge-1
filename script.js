42c377d37a3b2f1831b6273cecfb7800f3431af5
Ecksi <jimmy.dechant@gmail.com>
2018-02-28 06:47:55 -0700

    Add fun clicks for the memems

---
+++ script.js
@@ -0,0 +1,21 @@
+$('.card-2').on('click', function() {
+  window.open('https://youtu.be/uE-1RPDqJAY?t=3s');
+})
+$('.card-3').on('click', function () {
+  window.open('https://youtu.be/miomuSGoPzI?t=41s');
+})
+$('.card-4').on('click', function () {
+  window.open('https://youtu.be/Ct6BUPvE2sM?t=49s');
+})
+$('.card-5').on('click', function () {
+  window.open('https://youtu.be/662JyNLVpxE?t=30s');
+})
+$('.card-6').on('click', function () {
+  window.open('https://youtu.be/Z3ZAGBL6UBA?t=1s');
+})
+$('.card-7').on('click', function () {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+})
+$('.card-8').on('click', function () {
+  window.open('https://www.youtube.com/watch?v=BBGEG21CGo0');
+})
